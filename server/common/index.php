f2a12edd5cf279e6549bff7dca92ac1d818d47ad
gRaY4449 <onlyforschoolclass378@gmail.com>
2022-08-01 14:51:32 +0900

    finish:start_cover

--- server/common/index.php
+++ server/common/index.php
@@ -5,3 +5,13 @@ require_once __DIR__ . '/common/functions.php';
 
 // データベースに接続
 $dbh = connect_db();
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<?php include_once __DIR__ . '/_head.php' ?>
+
+<body>
+    
+</body>
+</html>
